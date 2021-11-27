f3d5e6285b68466c41892da627f44acf109d19af
Hugh Nguyen <hhnguyen255@gmail.com>
2021-11-27 15:35:07 +1100

    initial commit

---
+++ src/scripts/index.ts
@@ -0,0 +1,50 @@
+const body = document.querySelector<HTMLBodyElement>('body')!;
+
+const toggleDarkMode = document.getElementById(
+  'toggle-dark-mode'
+)! as HTMLInputElement;
+
+function switchDarkMode(status: string) {
+  const element = body;
+
+  if (status === 'off') {
+    element.classList.remove('dark-mode');
+    localStorage.removeItem('darkMode');
+    return;
+  }
+
+  if (status === 'on') {
+    if (element.classList.contains('dark-mode')) return;
+    element.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'on');
+  }
+}
+
+// manual turn on dark mode
+toggleDarkMode.addEventListener('change', function () {
+  if (this.checked) {
+    console.log('dark mode on');
+    switchDarkMode('on');
+    return;
+  }
+
+  console.log('dark mode off');
+  switchDarkMode('off');
+});
+
+// auto dark mode upon load
+const hour = new Date().getHours();
+if (hour < 6 || hour > 19 || localStorage.getItem('darkMode') === 'on') {
+  toggleDarkMode.checked = true;
+  switchDarkMode('on');
+}
+
+const reactInput = document.getElementById('react') as HTMLInputElement;
+reactInput.addEventListener('change', function () {
+  if (this.checked) {
+    console.log('checked');
+    return;
+  }
+
+  console.log('uncheck');
+});
